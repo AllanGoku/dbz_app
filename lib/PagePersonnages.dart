3d4925d4b6ae3268544abfa1638a530a1fe61818
Allan Vanrossem <102809467+AllanGoku@users.noreply.github.com>
2023-04-28 14:04:36 +0200

    Création page personnages + drawer pour toutes les pages

--- lib/PagePersonnages.dart
+++ lib/PagePersonnages.dart
@@ -1,50 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class PagePersonnages extends StatelessWidget {
   PagePersonnages({super.key});
+  Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
 
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Page de connexion"),
+      appBar: AppBar(
+        title: Text("Page des personnages"),
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
+          },
         ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-              ),
-              TextFormField(
-                maxLines: 1,
-                minLines: 1,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Adresse mail',
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(2.5),
-              ),
-              TextFormField(
-                obscureText: true,
-                maxLines: 1,
-                minLines: 1,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Mot de passe',
-                ),
-              ),
-              MaterialButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/second');
-                },
-                child: Text("Page Accueil"),
-                color: Colors.red,
-              ),
-            ],
+        automaticallyImplyLeading: false,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('Déconnexion'),
+                      content: Text('Voulez-vous vous déconnecter ?'),
+                      actions: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text('Non'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            _prefs.then((prefs) => {
+                                  prefs.remove('mailConnexion'),
+                                  prefs.remove('pseudoConnexion')
+                                });
+                            Navigator.pushReplacementNamed(context, '/');
+                          },
+                          child: Text('Oui'),
+                        ),
+                      ],
+                    );
+                  });
+            },
           ),
-        ));
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Page des personnages',
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/monDrawer.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MonDrawer extends StatelessWidget {
+  const MonDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.orange[600],
+              image: DecorationImage(
+                image: AssetImage('images/background_dragon_ball.jpg'),
+                fit: BoxFit.cover,
+                alignment: Alignment.centerRight,
+              ),
+            ),
+            child: null,
+          ),
+          ListTile(
+            leading: Image.asset("images/icone_personnages.jpg"),
+            title: Text(
+              'Personnages',
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+            onTap: () {
+              Navigator.pushNamed(context, '/personnages');
+              // Mettre ici le code à exécuter quand l'utilisateur clique sur l'option "Personnages"
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () {
+              // Mettre ici le code à exécuter quand l'utilisateur clique sur l'option "Settings"
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}

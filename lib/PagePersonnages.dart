07571b4a4ce6ca072562dbda8d8272b812c3c0f5
Allan Vanrossem <102809467+AllanGoku@users.noreply.github.com>
2023-05-10 16:59:52 +0200

    Ajout de l'API superHeroApi

--- lib/PagePersonnages.dart
+++ lib/PagePersonnages.dart
@@ -1,6 +1,8 @@
 import 'package:dbz_app/monDrawer.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 bool showText = false;
 Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
@@ -14,8 +16,30 @@ class PagePersonnages extends StatefulWidget {
 }
 
 class _PagePersonnagesState extends State<PagePersonnages> {
+
+  Future<Map<String,dynamic>?> fetchJSONData(String personnage) async {
+    final response = await http.get(Uri.parse(
+        'https://superheroapi.com/api/1549323725556590/search/$personnage'));
+    if (response.statusCode == 200) {
+      if (response.body ==
+          '{"response":"error","error":"character with given name not found"}') {
+        print('Personnage non trouvé');
+        throw Exception('Personnage non trouvé');
+      } else {
+        print(
+            jsonDecode(response.body)['results'][0]['biography']['full-name']);
+        return jsonDecode(response.body);
+      }
+    } else {
+      print('Failed to load data');
+      throw Exception('Failed to load data');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final cardHeight = height * 0.4;
     return Scaffold(
         appBar: AppBar(
           title: Text("Page d'accueil"),
@@ -67,39 +91,60 @@ class _PagePersonnagesState extends State<PagePersonnages> {
         ),
         drawer: MonDrawer(),
         body: GridView.count(
-          crossAxisCount: 2,
-          crossAxisSpacing: 3,
+          crossAxisCount: 1,
+          mainAxisSpacing: 15,
           shrinkWrap: true,
           children: <Widget>[
             Card(
-              child: Expanded(
-                  child: Column(children: <Widget>[
-                Image.asset('images/goku.png'),
-                Text('Goku'),
-                if (showText)
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  FutureBuilder<Map<String,dynamic>?>(
+                    future: fetchJSONData("goku"),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<Map<String,dynamic>?> snapshot) {
+                      if (snapshot.hasData) {
+                        return Image.network('${snapshot.data!['results'][0]['image']['url']}',
+                            height: cardHeight * 0.7);
+                      } else if (snapshot.hasError) {
+                        return Text(
+                            'Une erreur est survenue : ${snapshot.error}');
+                      } else {
+                        return Text('Chargement en cours...');
+                      }
+                    },
+                  ),
+                  //Image.asset('images/goku2.png', height: cardHeight * 0.7),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Text(
-                      "TROP BIEN C'EST GOKU",
+                      'Goku',
                       style: TextStyle(
-                        fontSize: 16,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      if (showText == true)
-                        showText = false;
-                      else
-                        showText = true;
-                    });
-                    print(showText);
-                  },
-                  child: Text(showText ? "Masquer" : "En savoir plus"),
-                ),
-              ])),
-            )
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: FutureBuilder<Map<String,dynamic>?>(
+                      future: fetchJSONData("goku"),
+                      builder: (BuildContext context,
+                          AsyncSnapshot<Map<String,dynamic>?> snapshot) {
+                        if (snapshot.hasData) {
+                          return Text('${snapshot.data!['results'][0]['biography']['full-name']}');
+                        } else if (snapshot.hasError) {
+                          return Text(
+                              'Une erreur est survenue : ${snapshot.error}');
+                        } else {
+                          return Text('Chargement en cours...');
+                        }
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ));
   }

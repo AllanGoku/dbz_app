0a870dba9a81643cf9653deba57bbd667faf8a41
Allan Vanrossem <102809467+AllanGoku@users.noreply.github.com>
2023-05-11 22:30:06 +0200

    Modification appbar par l'utilisation de la nouvelle classe et suppression de fichier inutile.

--- lib/PageAccueil.dart
+++ lib/PageAccueil.dart
@@ -1,10 +1,10 @@
+import 'package:dbz_app/monAppBar.dart';
 import 'package:dbz_app/monDrawer.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class PageAccueil extends StatelessWidget {
   PageAccueil({super.key});
-  Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   // This widget is the root of your application.
 
   Future<String?> recupMail() async {
@@ -20,48 +20,7 @@ class PageAccueil extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Page d'accueil"),
-        leading: Builder(
-          builder: (BuildContext context) {
-            return IconButton(
-              icon: const Icon(Icons.menu),
-              onPressed: () { Scaffold.of(context).openDrawer(); },
-              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-            );
-          },
-        ),
-        automaticallyImplyLeading: false,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.logout),
-            onPressed: () {
-              showDialog(context: context, builder: (BuildContext context) {
-                return AlertDialog(
-                  title: Text('Déconnexion'),
-                  content: Text('Voulez-vous vous déconnecter ?'),
-                  actions: [
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text('Non'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        _prefs.then((prefs) => {prefs.remove('mailConnexion'),
-                          prefs.remove('pseudoConnexion')});
-                        Navigator.pushReplacementNamed(context, '/');
-                      },
-                      child: Text('Oui'),
-                    ),
-                  ],
-                );
-              });
-            },
-          ),
-        ],
-      ),
+      appBar: MonAppBar(),
       drawer: MonDrawer(),
       body: Center(
         child: Column(

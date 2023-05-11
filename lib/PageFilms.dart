b45c876d2a848b16d8750e32df0a3feb8f37edfe
Allan Vanrossem <102809467+AllanGoku@users.noreply.github.com>
2023-05-11 21:47:45 +0200

    Ajout de la listView avec le titre et la description des films.

--- lib/PageFilms.dart
+++ lib/PageFilms.dart
@@ -1,11 +1,10 @@
+import 'package:dbz_app/monAppBar.dart';
 import 'package:dbz_app/monDrawer.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 bool showText = false;
-Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
 
 class PageFilms extends StatefulWidget {
   PageFilms({super.key});
@@ -16,24 +15,24 @@ class PageFilms extends StatefulWidget {
 }
 
 class _PageFilmsState extends State<PageFilms> {
+  Map<int, dynamic>? _films;
 
-  Map<String,dynamic>? _films;
-
-  Future<Map<String, String>?> obtenirFilms() async {
-    final response = await http.get(Uri.parse('https://anime-db.p.rapidapi.com/anime?page=1&size=15&search=Dragon%20ball%20z&sortBy=title&sortOrder=asc&types=Movie'),
+  Future<Map<int, dynamic>> obtenirFilms() async {
+    final response = await http.get(
+        Uri.parse(
+            'https://anime-db.p.rapidapi.com/anime?page=1&size=15&search=Dragon%20ball%20z&sortBy=title&sortOrder=asc&types=Movie'),
         headers: {
-          'X-Rapidapi-Key': 'a4927f5b41msh452a6972b3b2e8ap136099jsn3cdae7586830',
+          'X-Rapidapi-Key':
+              'a4927f5b41msh452a6972b3b2e8ap136099jsn3cdae7586830',
           'X-Rapidapi-Host': 'anime-db.p.rapidapi.com'
         });
     if (response.statusCode == 200) {
-      final data = jsonDecode(response.body)['data'];
-      final films = <String, String>{};
+      final data = jsonDecode(response.body);
+      final films = <int, dynamic>{};
 
-      for (final film in data) {
-        final title = film['title'] as String;
-        final description = film['synopsis'] as String;
-
-        films[title] = description;
+      for (int i = 0; i < data['data'].length; i++) {
+        final film = data['data'][i];
+        films[i] = film;
       }
       print(films);
       return films;
@@ -43,21 +42,27 @@ class _PageFilmsState extends State<PageFilms> {
     }
   }
 
-  void initState(){
-    super.initState();
-    obtenirFilms().then((value) => setState(() {
-      _films = value;
-    }));
+  List<String>? obtenirTitresDesFilms() {
+    List<String> result = [];
+    _films!.forEach((key, value) {
+      print(value['title']);
+      result.add(value['title']);
+    });
+    return result;
   }
 
-  List<String> _items = [
-    'Item 1',
-    'Item 2',
-    'Item 3',
-    'Item 4',
-  ];
+  void initState() {
+    super.initState();
+    obtenirFilms().then((data) {
+      setState(() {
+        _films = data;
+      });
+    });
+    //print(_films);
+    //_titres = obtenirTitresDesFilms();
+  }
 
-  List<bool> _isExpandedList = List.generate(4, (index) => false);
+  List<bool> _isExpandedList = List.generate(15, (index) => false);
 
   void _toggleExpanded(int index) {
     setState(() {
@@ -74,60 +79,64 @@ class _PageFilmsState extends State<PageFilms> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Films'),
-        ),
+        appBar:MonAppBar(),
         drawer: MonDrawer(),
-        body: ListView.builder(
-          itemCount: _items.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Column(
-              children: <Widget>[
-                InkWell(
-                  onTap: () {
-                    _toggleExpanded(index);
-                  },
-                  child: Container(
-                    height: 50,
-                    padding: EdgeInsets.symmetric(horizontal: 16.0),
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      _items[index],
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18.0,
-                      ),
-                    ),
-                  ),
-                ),
-                if (_isExpandedList[index])
-                  Container(
-                    child: Column(children: [
-                      Text(
-                        'Contenu ${_items[index]}',
-                        style: TextStyle(
-                          fontSize: 16.0,
-                        ),
-                      ),
-                      FutureBuilder<Map<String,dynamic>?>(
-                        future: obtenirFilms(),
-                        builder: (BuildContext context,
-                            AsyncSnapshot<Map<String,dynamic>?> snapshot) {
-                          if (snapshot.hasData) {
-                            return Text('${snapshot.data}');
-                          } else if (snapshot.hasError) {
-                            return Text(
-                                'Une erreur est survenue : ${snapshot.error}');
-                          } else {
-                            return Text('Chargement en cours...');
-                          }
-                        },
-                      ),
-                    ]),
-                  ),
-              ],
-            );
-          },
-        ));
+        body: _films == null
+            ? const CircularProgressIndicator()
+            : ListView.builder(
+                itemCount: _films?.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: InkWell(
+                          onTap: () {
+                            _toggleExpanded(index);
+                          },
+                          child: Container(
+                            child: Column(
+                              children: <Widget>[
+                                InkWell(
+                                  onTap: () {
+                                    _toggleExpanded(index);
+                                  },
+                                  child: Container(
+                                    height: 50,
+                                    padding:
+                                        EdgeInsets.symmetric(horizontal: 16.0),
+                                    alignment: Alignment.centerLeft,
+                                    child: Text(
+                                      _films![index]!['title'],
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18.0,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                if (_isExpandedList[index])
+                                  Container(
+                                    child: Column(children: [
+                                      Text(
+                                        'Description : ', //${_items[index]}
+                                        style: TextStyle(
+                                          fontSize: 16.0,
+                                        ),
+                                      ),
+                                      Text(
+                                        '${_films![index]!['synopsis']}',
+                                      ),
+                                    ]),
+                                  ),
+                              ],
+                            ),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.black,
+                                width: 2,
+                              ),
+                            ),
+                          )));
+                },
+              ) /**/);
   }
 }

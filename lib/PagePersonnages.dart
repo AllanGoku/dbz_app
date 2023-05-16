c816fd41798cafec356b88682163df2b45218f92
Allan Vanrossem <102809467+AllanGoku@users.noreply.github.com>
2023-05-16 16:24:21 +0200

    Modification des méthodes et utilisations afin de permettre de réduire le nombre de requête envoyé à l'API d'un gridView Builder pour réduire le code.

--- lib/PagePersonnages.dart
+++ lib/PagePersonnages.dart
@@ -6,7 +6,6 @@ import 'package:http/http.dart' as http;
 
 bool showText = false;
 
-
 class PagePersonnages extends StatefulWidget {
   PagePersonnages({super.key});
 
@@ -16,8 +15,8 @@ class PagePersonnages extends StatefulWidget {
 }
 
 class _PagePersonnagesState extends State<PagePersonnages> {
-
-  Future<Map<String,dynamic>?> obtenirInformationsPersonnage(String personnage) async {
+  Future<Map<String, dynamic>?> obtenirInformationsPersonnage(
+      String personnage) async {
     final response = await http.get(Uri.parse(
         'https://superheroapi.com/api/1549323725556590/search/$personnage'));
     if (response.statusCode == 200) {
@@ -37,18 +36,16 @@ class _PagePersonnagesState extends State<PagePersonnages> {
   }
 
   Future<String?> obtenirImagePersonnage(String personnage) async {
-    var info =  (await obtenirInformationsPersonnage(personnage));
+    var info = (await obtenirInformationsPersonnage(personnage));
     var id = info!['results'][0]['id'];
-    final response = await http.get(Uri.parse(
-        'https://superheroapi.com/api/1549323725556590/$id/image'));
+    final response = await http.get(
+        Uri.parse('https://superheroapi.com/api/1549323725556590/$id/image'));
     if (response.statusCode == 200) {
-      if (response.body ==
-          '{ "response": "error","error": "invalid id" }') {
+      if (response.body == '{ "response": "error","error": "invalid id" }') {
         print('Personnage non trouvé');
         throw Exception('Personnage non trouvé');
       } else {
-        print(
-            jsonDecode(response.body)["url"]);
+        print(jsonDecode(response.body)["url"]);
         return jsonDecode(response.body)["url"];
       }
     } else {
@@ -57,6 +54,20 @@ class _PagePersonnagesState extends State<PagePersonnages> {
     }
   }
 
+  List<String> listPersonnages = ["Goku", "Vegeta"];
+
+  List<dynamic> _personnages = [];
+
+  void initState() {
+    super.initState();
+    for (int i = 0; i < listPersonnages.length; i++) {
+      obtenirInformationsPersonnage(listPersonnages[i]).then((data) {
+        setState(() {
+          _personnages.add(data);
+        });
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -65,62 +76,46 @@ class _PagePersonnagesState extends State<PagePersonnages> {
     return Scaffold(
         appBar: MonAppBar(titre: "Personnages"),
         drawer: MonDrawer(),
-        body: GridView.count(
-          crossAxisCount: 1,
-          mainAxisSpacing: 15,
-          shrinkWrap: true,
-          children: <Widget>[
-            Card(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  FutureBuilder<String?>(
-                    future: obtenirImagePersonnage("goku"),
-                    builder: (BuildContext context,
-                        AsyncSnapshot<String?> snapshot) {
-                      if (snapshot.hasData) {
-                        return Image.network('${snapshot.data}',
-                            height: cardHeight * 0.7);
-                      } else if (snapshot.hasError) {
-                        return Text(
-                            'Une erreur est survenue : ${snapshot.error}');
-                      } else {
-                        return Text('Chargement en cours...');
-                      }
-                    },
-                  ),
-                  //Image.asset('images/goku2.png', height: cardHeight * 0.7),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Goku',
-                      style: TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: FutureBuilder<Map<String,dynamic>?>(
-                      future: obtenirInformationsPersonnage("goku"),
-                      builder: (BuildContext context,
-                          AsyncSnapshot<Map<String,dynamic>?> snapshot) {
-                        if (snapshot.hasData) {
-                          return Text('${snapshot.data!['results'][0]['biography']['full-name']}');
-                        } else if (snapshot.hasError) {
-                          return Text(
-                              'Une erreur est survenue : ${snapshot.error}');
-                        } else {
-                          return Text('Chargement en cours...');
-                        }
-                      },
-                    ),
-                  ),
+        body: _personnages == null
+            ? Center(
+                child: Column(
+                children: [
+                  Text('Chargement des personnages...'),
+                  CircularProgressIndicator(),
                 ],
-              ),
-            ),
-          ],
-        ));
+              ))
+            : GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 1),
+                itemCount: _personnages.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Image.network(
+                            _personnages[index]['results'][0]['image']['url'],
+                            height: cardHeight * 0.7),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            _personnages[index]['results'][0]['name'],
+                            style: TextStyle(
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Text(
+                            _personnages[index]['results'][0]['biography']
+                                ['full-name'],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }));
   }
 }
